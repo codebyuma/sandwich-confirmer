d13b3435b75e075452f74bb99d8a6011154ffc2e
Uma Chandran <uma.chandran@rangle.io>
2017-12-27 14:08:57 -0500

    minor changes

--- api/index.js
+++ api/index.js
@@ -2,24 +2,19 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/ingredients', function (req, res) {
-    fs.readFile('./data/ingredients/ingredients.json', (err, result) => {
+app.get('/ingredients', (req, res) => {
+    fs.readFile('./data/ingredients/ingredients.json', 'utf8', (err, result) => {
         res.send(result);
     });
 });
 
-app.get('/ingredients/details', function (req, res) {
-    fs.readFile('./data/ingredients/ingredientDetails.json', (err, result) => {
+app.get('/ingredients/details', (req, res) => {
+    fs.readFile('./data/ingredients/ingredientDetails.json', 'utf8', (err, result) => {
         res.send(result);
     });
 });
 
-
-app.listen(4200, function () {
-  console.log('Example app listening on port 4200!');
-});
+app.listen(4200, () => console.log('Example app listening on port 4200!'));
 
