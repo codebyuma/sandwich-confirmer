f46541f1fb955348440b4a6ed29a1632ec933e0f
Uma Chandran <uma.chandran@rangle.io>
2017-12-27 14:05:34 -0500

    adding more data

---
+++ api/data/ingredients/ingredientDetails.js
@@ -0,0 +1,11 @@
+const ingredientDetails = [
+    { 
+        id: 1,
+        name: 'whole grain bread',
+        lowCrustCoverage: 5,
+        lengthVsWidthEquality: 5,
+        slicedToCompletion: 5,
+        squareToCircle: 1,
+        savoury: 10
+    }
+];
--- api/index.js
+++ api/index.js
@@ -3,10 +3,21 @@ const fs = require('fs');
 const app = express();
 
 app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/ingredients', function (req, res) {
     fs.readFile('./data/ingredients/ingredients.json', (err, result) => {
         res.send(result);
     });
 });
+
+app.get('/ingredients/details', function (req, res) {
+    fs.readFile('./data/ingredients/ingredientDetails.json', (err, result) => {
+        res.send(result);
+    });
+});
+
 
 app.listen(4200, function () {
   console.log('Example app listening on port 4200!');
